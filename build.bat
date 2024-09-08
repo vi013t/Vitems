4d8c1a905b3c02222f06adf96be8fc103fee0af5
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-09-08 10:29:14 -0400

    Fix build script, update assets

--- build.bat
+++ build.bat
@@ -6,14 +6,14 @@ for /f %%a in ('echo prompt $E^| cmd') do set "ESC=%%a"
 echo:
 echo %ESC%[0;32mBuilding%ESC%[0;0m Vitems.
 
-:: Build the Visual Studio project
-echo | set /p dummyName=%ESC%[0;32m    Building%ESC%[0;0m solution...
-dotnet build ./Vitems/RoR2Mods.sln 1> nul || exit /b
-echo %ESC%[0;32m Done%ESC%[0;0m.
-
 :: Build the Unity assets
 echo | set /p dummyName=%ESC%[0;32m    Building%ESC%[0;0m Unity AssetBundle...
 "C:\Program Files\Unity\Hub\Editor\2023.2.9f1\Editor\Unity.exe" -batchmode -quit -projectPath "./VitemsAssets" -executeMethod BuildAssets.BuildAllAssetBundles
+echo %ESC%[0;32m Done%ESC%[0;0m.
+
+:: Build the Visual Studio project
+echo | set /p dummyName=%ESC%[0;32m    Building%ESC%[0;0m solution...
+dotnet build ./Vitems/RoR2Mods.sln 1> nul || exit /b
 echo %ESC%[0;32m Done%ESC%[0;0m.
 
 :: Copy over the DLL to R2ModMan
@@ -25,4 +25,4 @@ echo %ESC%[0;32m Done%ESC%[0;0m.
 echo %ESC%[0;32mFinished %ESC%[0;0mBuilding Vitems.
 echo:
 
-echo %ESC%[0;32mClick %ESC%[0;0m"Launch Modded" from R2ModMan to try the mod.
+echo %ESC%[0;32mClick %ESC%[0;0m"Start Modded" from R2ModMan to try the mod and test changes.
